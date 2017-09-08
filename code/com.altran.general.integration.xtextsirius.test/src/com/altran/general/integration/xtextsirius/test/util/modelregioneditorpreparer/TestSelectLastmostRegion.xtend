7c6b23939c6a542b7b25f1c2acf05b8ac7d3eb96
Niko Stotz <niko.stotz@altran.com>
2017-09-08 15:36:47 +0200

    added tests for ModelRegionEditorPreparer.selectLastmostRegion

--- code/com.altran.general.integration.xtextsirius.test/src/com/altran/general/integration/xtextsirius/test/util/modelregioneditorpreparer/TestSelectLastmostRegion.xtend
+++ code/com.altran.general.integration.xtextsirius.test/src/com/altran/general/integration/xtextsirius/test/util/modelregioneditorpreparer/TestSelectLastmostRegion.xtend
@@ -1,11 +1,55 @@
 package com.altran.general.integration.xtextsirius.test.util.modelregioneditorpreparer
 
+import java.util.Collections
+import java.util.NoSuchElementException
+import java.util.Random
+import org.junit.Test
+
 import static org.junit.Assert.*
 
 class TestSelectLastmostRegion extends AModelRegionEditorPreparer {
-	@org.junit.Test
-	def simple_region() {
-		assertTrue(true)
+	@Test(expected=NoSuchElementException)
+	def void emptyRegionList() {
+		val preparer = fakePreparer
+
+		preparer.selectLastmostRegion(emptySet)
+	}
+
+	@Test
+	def singleEntry() {
+		val model = defaultModel
+
+		val event = model.events.head
+
+		val preparer = fakePreparer
+
+		val rootRegion = getRootRegion(event)
+		val eventRegion = rootRegion.regionForEObject(event)
+		val semanticRegion = eventRegion.semanticRegions.head
+
+		val lastmost = preparer.selectLastmostRegion(#[semanticRegion].toSet)
+
+		assertSame(semanticRegion, lastmost)
+	}
+
+	@Test
+	def manyEntries() {
+		val model = defaultModel
+
+		val event = model.events.head
+
+		val preparer = fakePreparer
+
+		val rootRegion = getRootRegion(event)
+		val eventRegion = rootRegion.regionForEObject(event)
+		val lastRegion = eventRegion.semanticRegions.last
+
+		val regions = eventRegion.semanticRegions.toList
+		Collections.shuffle(regions, new Random(31337))
+
+		val lastmost = preparer.selectLastmostRegion(regions.toSet)
+
+		assertSame(lastRegion, lastmost)
 	}
 
 }
--- code/com.altran.general.integration.xtextsirius.test/xtend-gen/com/altran/general/integration/xtextsirius/test/util/modelregioneditorpreparer/TestSelectLastmostRegion.java
+++ code/com.altran.general.integration.xtextsirius.test/xtend-gen/com/altran/general/integration/xtextsirius/test/util/modelregioneditorpreparer/TestSelectLastmostRegion.java
@@ -1,13 +1,52 @@
 package com.altran.general.integration.xtextsirius.test.util.modelregioneditorpreparer;
 
 import com.altran.general.integration.xtextsirius.test.util.modelregioneditorpreparer.AModelRegionEditorPreparer;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import org.eclipse.xtext.example.fowlerdsl.statemachine.Event;
+import org.eclipse.xtext.example.fowlerdsl.statemachine.Statemachine;
+import org.eclipse.xtext.formatting2.regionaccess.IEObjectRegion;
+import org.eclipse.xtext.formatting2.regionaccess.ISemanticRegion;
+import org.eclipse.xtext.formatting2.regionaccess.ITextRegionAccess;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.junit.Assert;
 import org.junit.Test;
 
 @SuppressWarnings("all")
 public class TestSelectLastmostRegion extends AModelRegionEditorPreparer {
+  @Test(expected = NoSuchElementException.class)
+  public void emptyRegionList() {
+    final AModelRegionEditorPreparer.AccessibleModelRegionEditorPreparer preparer = this.getFakePreparer();
+    preparer.selectLastmostRegion(CollectionLiterals.<ISemanticRegion>emptySet());
+  }
+  
   @Test
-  public void simple_region() {
-    Assert.assertTrue(true);
+  public void singleEntry() {
+    final Statemachine model = this.getDefaultModel();
+    final Event event = IterableExtensions.<Event>head(model.getEvents());
+    final AModelRegionEditorPreparer.AccessibleModelRegionEditorPreparer preparer = this.getFakePreparer();
+    final ITextRegionAccess rootRegion = this.getRootRegion(event);
+    final IEObjectRegion eventRegion = rootRegion.regionForEObject(event);
+    final ISemanticRegion semanticRegion = IterableExtensions.<ISemanticRegion>head(eventRegion.getSemanticRegions());
+    final ISemanticRegion lastmost = preparer.selectLastmostRegion(IterableExtensions.<ISemanticRegion>toSet(Collections.<ISemanticRegion>unmodifiableList(CollectionLiterals.<ISemanticRegion>newArrayList(semanticRegion))));
+    Assert.assertSame(semanticRegion, lastmost);
+  }
+  
+  @Test
+  public void manyEntries() {
+    final Statemachine model = this.getDefaultModel();
+    final Event event = IterableExtensions.<Event>head(model.getEvents());
+    final AModelRegionEditorPreparer.AccessibleModelRegionEditorPreparer preparer = this.getFakePreparer();
+    final ITextRegionAccess rootRegion = this.getRootRegion(event);
+    final IEObjectRegion eventRegion = rootRegion.regionForEObject(event);
+    final ISemanticRegion lastRegion = IterableExtensions.<ISemanticRegion>last(eventRegion.getSemanticRegions());
+    final List<ISemanticRegion> regions = IterableExtensions.<ISemanticRegion>toList(eventRegion.getSemanticRegions());
+    Random _random = new Random(31337);
+    Collections.shuffle(regions, _random);
+    final ISemanticRegion lastmost = preparer.selectLastmostRegion(IterableExtensions.<ISemanticRegion>toSet(regions));
+    Assert.assertSame(lastRegion, lastmost);
   }
 }
