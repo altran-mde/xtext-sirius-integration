0c173fa4984b0ba2c6c3c7d049579b9e896405bb
Niko Stotz <niko.stotz@altran.com>
2017-09-11 18:02:17 +0200

    added javadoc to SemanticElementLocation

--- code/com.altran.general.integration.xtextsirius/src/com/altran/general/integration/xtextsirius/internal/SemanticElementLocation.java
+++ code/com.altran.general.integration.xtextsirius/src/com/altran/general/integration/xtextsirius/internal/SemanticElementLocation.java
@@ -6,17 +6,36 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jdt.annotation.NonNull;
 
+/**
+ * Stores the location of an {@link EObject} in a robust way.
+ *
+ * <p>
+ * We can store the location of any EObject or a non-existing EObject, given its
+ * parent, containing feature and index (if the containing feature is a
+ * collection). The location can be resolved even if there are major changes to
+ * the target or descendants. It might not be resolvable if the model changed
+ * outside of the target.
+ * </p>
+ *
+ * <p>
+ * We do not keep any expensive / highly connected state of the target; We keep
+ * a reference to the target's metamodel. This should reduce memory leaks.
+ * </p>
+ *
+ * @author nstotz
+ *
+ */
 public class SemanticElementLocation {
-	
+
 	private String uriFragment;
 	private String parentUriFragment;
 	private EStructuralFeature containingFeature;
 	private int index;
-
+	
 	public SemanticElementLocation(final @NonNull EObject semanticElement) {
 		storeLocation(semanticElement);
 	}
-
+	
 	public SemanticElementLocation(
 			final @NonNull String uriFragment,
 			final @NonNull String parentUriFragment,
@@ -27,7 +46,7 @@ public class SemanticElementLocation {
 		this.containingFeature = containingFeature;
 		this.index = index;
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	private void storeLocation(final @NonNull EObject semanticElement) {
 		final Resource resource = semanticElement.eResource();
@@ -39,14 +58,14 @@ public class SemanticElementLocation {
 					.eGet(this.containingFeature)).indexOf(semanticElement);
 		}
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	public EObject resolve(final @NonNull Resource resource) {
 		final EObject result = resource.getEObject(this.uriFragment);
 		if (result != null) {
 			return result;
 		}
-
+		
 		final Object containingElement = resource.getEObject(this.parentUriFragment).eGet(this.containingFeature);
 		if (this.containingFeature.isMany()) {
 			return ((EList<EObject>) containingElement).get(this.index);
