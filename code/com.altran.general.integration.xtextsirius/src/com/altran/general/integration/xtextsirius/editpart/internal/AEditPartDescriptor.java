5cd3aa346c764d49b61f8b0f037711aad4ce0926
Niko Stotz <niko.stotz@altran.com>
2017-09-13 15:25:54 +0200

    improved validation of editPart definition (make sure all required elements are there)

--- code/com.altran.general.integration.xtextsirius/src/com/altran/general/integration/xtextsirius/editpart/internal/AEditPartDescriptor.java
+++ code/com.altran.general.integration.xtextsirius/src/com/altran/general/integration/xtextsirius/editpart/internal/AEditPartDescriptor.java
@@ -1,20 +1,27 @@
 package com.altran.general.integration.xtextsirius.editpart.internal;
 
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.sirius.diagram.DDiagramElement;
+import org.eclipse.sirius.diagram.description.DiagramElementMapping;
+import org.eclipse.sirius.diagram.description.tool.DirectEditLabel;
+import org.eclipse.sirius.viewpoint.description.RepresentationElementMapping;
+import org.eclipse.sirius.viewpoint.description.tool.InitialOperation;
+import org.eclipse.sirius.viewpoint.description.tool.SetValue;
 import org.yakindu.base.xtext.utils.gmf.directedit.IXtextAwareEditPart;
 
 import com.altran.general.integration.xtextsirius.editpart.IXtextDirectEditConfiguration;
 import com.altran.general.integration.xtextsirius.internal.IDescriptor;
 
 public abstract class AEditPartDescriptor implements IDescriptor {
-
+	
 	private final String identifier;
 	private final boolean multiLine;
 	private final IXtextDirectEditConfiguration config;
-	
+
 	public AEditPartDescriptor(
 			final @Nullable String identifier,
 			final boolean multiLine,
@@ -23,29 +30,46 @@ public abstract class AEditPartDescriptor implements IDescriptor {
 		this.multiLine = multiLine;
 		this.config = config;
 	}
-	
+
 	@Override
 	public @NonNull String getIdentifier() {
 		return this.identifier;
 	}
-	
+
 	@Override
 	public boolean isMultiLine() {
 		return this.multiLine;
 	}
-	
+
 	public @NonNull IXtextDirectEditConfiguration getConfig() {
 		return this.config;
 	}
-	
+
 	@Override
 	public boolean isValid() {
 		return StringUtils.isNotBlank(getIdentifier()) && getConfig() != null;
 	}
-
+	
 	public boolean matches(final @NonNull View view) {
-		return true;
-	}
+		final EObject element = view.getElement();
+		if (element instanceof DDiagramElement) {
+			final RepresentationElementMapping mapping = ((DDiagramElement) element).getMapping();
+			if (mapping instanceof DiagramElementMapping) {
+				final DirectEditLabel directEdit = ((DiagramElementMapping) mapping).getLabelDirectEdit();
+				if (directEdit != null) {
+					final InitialOperation initialOperation = directEdit.getInitialOperation();
+					if (initialOperation != null) {
+						if (initialOperation.getFirstModelOperations() instanceof SetValue) {
+							return true;
+						}
+					}
+				}
+			}
 
+		}
+
+		return false;
+	}
+	
 	public abstract @NonNull IXtextAwareEditPart createEditPart(final @NonNull View view);
 }
