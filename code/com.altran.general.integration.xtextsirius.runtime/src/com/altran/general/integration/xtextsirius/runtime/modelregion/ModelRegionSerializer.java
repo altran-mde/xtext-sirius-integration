ff01a6047e77db51ac729714abaea381b0c52175
Tjalling Ran <tjalling.ran@altran.com>
2018-11-05 21:20:41 +0100

    Refactor ModelRegionSerializer

--- code/com.altran.general.integration.xtextsirius.runtime/src/com/altran/general/integration/xtextsirius/runtime/modelregion/ModelRegionSerializer.java
+++ code/com.altran.general.integration.xtextsirius.runtime/src/com/altran/general/integration/xtextsirius/runtime/modelregion/ModelRegionSerializer.java
@@ -1,16 +1,17 @@
 /**
  * Copyright (C) 2018 Altran Netherlands B.V.
- * 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 package com.altran.general.integration.xtextsirius.runtime.modelregion;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
@@ -18,42 +19,40 @@ import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.formatting2.regionaccess.ITextRegionAccess;
 
 import com.altran.general.integration.xtextsirius.runtime.ignoredfeature.IgnoredFeatureAdapter;
 import com.altran.general.integration.xtextsirius.runtime.util.FeaturePathUtil;
+import com.google.common.collect.Lists;
 
 @SuppressWarnings("restriction")
 public class ModelRegionSerializer {
 	private final ModelRegionEditorPreparer preparer;
-
+	
 	public ModelRegionSerializer(final ModelRegionEditorPreparer preparer) {
 		this.preparer = preparer;
 	}
-
+	
 	public ITextRegionAccess serialize(final @NonNull EObject rootContainer) {
-		EClass eClass;
-		if (this.preparer.getSemanticElement() != null) {
-			eClass = this.preparer.getSemanticElement().eClass();
-		} else {
-			eClass = (EClass) this.preparer.getSemanticElementFeature().getEType();
-		}
-
-		final List<? extends EObject> allInstances = EcoreUtil2.getAllContentsOfType(rootContainer,
-				(Class<? extends EObject>) eClass.getInstanceClass());
-		for (final EObject instance : allInstances) {
-			markIgnoredFeatures("", instance);
-		}
-		
-		
-		// if (this.preparer.getSemanticElement() != null) {
-		// markIgnoredFeatures("", this.preparer.getSemanticElement());
-		// } else {
-		// markIgnoredFeature(this.preparer.getSemanticElementFeature(), "",
-		// this.preparer.getParent());
-		// }
+		markIgnoredFeatures(rootContainer);
 		return this.preparer.getSerializer().serializeToRegions(rootContainer);
+	}
+	
+	protected void markIgnoredFeatures(final @NonNull EObject rootContainer) {
+		final EClass semanticElementEClass = getSemanticElementEClass();
+		Lists.newArrayList(rootContainer.eAllContents()).stream()
+				.filter(eObject -> eObject.eClass() == semanticElementEClass)
+				.forEach(eObject -> markIgnoredFeatures("", eObject));
+	}
+	
+	protected void markIgnoredFeatures(final @NonNull String prefix, final @NonNull EObject element) {
+		element.eClass().getEAllStructuralFeatures().stream()
+				.forEach(feature -> markIgnoredFeature(feature, prefix, element));
+	}
+	
+	protected void markIgnoredFeatures(final @NonNull String prefix, final @NonNull Collection<EObject> element) {
+		element.stream()
+				.forEach(eObject -> markIgnoredFeatures(prefix, eObject));
 	}
 	
 	protected void markIgnoredFeature(final @NonNull EStructuralFeature feature, final @NonNull String prefix,
@@ -71,16 +70,24 @@ public class ModelRegionSerializer {
 			if (child instanceof EObject) {
 				markIgnoredFeatures(featurePath, (EObject) child);
 			} else if (feature.isMany() && child instanceof Collection) {
-				for (final EObject c : ((Collection<EObject>) child)) {
-					markIgnoredFeatures(featurePath, c);
-				}
+				final List<EObject> children = getEObjectsInCollection((Collection<?>) child);
+				markIgnoredFeatures(prefix, children);
 			}
 		}
 	}
-
-	protected void markIgnoredFeatures(final @NonNull String prefix, final @NonNull EObject element) {
-		for (final EStructuralFeature feature : element.eClass().getEAllStructuralFeatures()) {
-			markIgnoredFeature(feature, prefix, element);
+	
+	protected EClass getSemanticElementEClass() {
+		if (this.preparer.getSemanticElement() != null) {
+			return this.preparer.getSemanticElement().eClass();
+		} else {
+			return (EClass) this.preparer.getSemanticElementFeature().getEType();
 		}
+	}
+	
+	protected List<EObject> getEObjectsInCollection(final Collection<?> collection) {
+		return collection.stream()
+				.filter(EObject.class::isInstance)
+				.map(EObject.class::cast)
+				.collect(Collectors.toList());
 	}
 }
