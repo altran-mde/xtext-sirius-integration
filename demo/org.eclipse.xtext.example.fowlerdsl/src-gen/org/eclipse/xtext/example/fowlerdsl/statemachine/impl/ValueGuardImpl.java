ac3f9d81292b69008c409d5ec47c769b789c189a
Niko Stotz <niko.stotz@altran.com>
2019-06-14 14:56:51 +0200

    re-added toString() implementations to fowlerdsl generated code

--- demo/org.eclipse.xtext.example.fowlerdsl/src-gen/org/eclipse/xtext/example/fowlerdsl/statemachine/impl/ValueGuardImpl.java
+++ demo/org.eclipse.xtext.example.fowlerdsl/src-gen/org/eclipse/xtext/example/fowlerdsl/statemachine/impl/ValueGuardImpl.java
@@ -190,4 +190,15 @@ public class ValueGuardImpl extends GuardImpl implements ValueGuard
     return super.eIsSet(featureID);
   }
 
+  /**
+   * @generated NOT
+   */
+	@Override
+	public String toString() {
+		if (eIsProxy()) {
+			return super.toString();
+		}
+		return this.cond != null ? this.cond.toString() : "?";
+	}
+	
 } //ValueGuardImpl
